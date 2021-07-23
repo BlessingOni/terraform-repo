b42819fb658718e8fb9e1eb409142dda69f8f151
BlessingOni <85324895+BlessingOni@users.noreply.github.com>
2021-07-22 22:10:45 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "jjtech" {
+resource "aws_instance" "jenkins-terraform" {
     ami = "ami-0233c2d874b811deb"
     instance_type = "t2.micro"
 
-tags {
+tags = {
   Name = "jenkins-terraform"
 }
 }
